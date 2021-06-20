03f0da28cf294aa93d6c608dc35bad410ab92b3d
Gaurav Krv <69639678+GauravKrv@users.noreply.github.com>
2021-06-20 20:45:51 +0530

    added function for gcd

--- DSA SERIES QUESTIONS/src/Mathematics/GCD.java
+++ DSA SERIES QUESTIONS/src/Mathematics/GCD.java
@@ -7,7 +7,13 @@ public class GCD {
         return gcd(b,a%b);
     }
 
+    static int lcm(int a,int b){
+        return a*b/gcd(a,b);
+    }
+
     public static void main(String[] args) {
-        System.out.println(gcd(14,21));
+        int a = 32, b = 64;
+        System.out.println("GCD OF "+a+" and "+b+ " = "+gcd(a,b));
+        System.out.println("LCM OF "+a+" and "+b+ " = "+lcm(a,b));
     }
 }
